a6a29511be482f3b0363d04c82ccfc0a312594f4
Michael <82292036+schumannlive@users.noreply.github.com>
2021-04-11 22:02:16 +0200

    Update save.js

--- save.js
+++ save.js
@@ -1,5 +1,3 @@
-///image save
-
 const download = require('image-downloader')
 
 setInterval(saveimg1, 1000*60*10)
@@ -65,3 +63,52 @@ download.image(options)
   })
   .catch((err) => console.error(err))
 }
+
+setInterval(saveimg5, 1000*60*10)
+
+function saveimg5(){
+
+const options = {
+  url: 'http://www.vlf.it/cumiana/last_E-VLF.jpg',
+  dest: './media/'                // will be saved to /path/to/dest/image.jpg
+}
+
+download.image(options)
+  .then(({ filename }) => {
+    console.log('Saved to', filename)  // saved to /path/to/dest/image.jpg
+  })
+  .catch((err) => console.error(err))
+}
+
+setInterval(saveimg6, 1000*60*10)
+
+function saveimg6(){
+
+const options = {
+  url: 'http://www.vlf.it/cumiana/last-geomar.jpg',
+  dest: './media/'                // will be saved to /path/to/dest/image.jpg
+}
+
+download.image(options)
+  .then(({ filename }) => {
+    console.log('Saved to', filename)  // saved to /path/to/dest/image.jpg
+  })
+  .catch((err) => console.error(err))
+}
+
+setInterval(saveimg7, 1000*60*10)
+
+function saveimg7(){
+
+const options = {
+  url: 'http://www.vlf.it/cumiana/last-marconi-multistrip-slow.jpg',
+  dest: './media/'                // will be saved to /path/to/dest/image.jpg
+}
+
+download.image(options)
+  .then(({ filename }) => {
+    console.log('Saved to', filename)  // saved to /path/to/dest/image.jpg
+  })
+  .catch((err) => console.error(err))
+}
+
